8836d23703e464b475e1ecb9d627261bbd53e7a0
GoncaloBatalha <goncalo.p.batalha@tecnico.ulisboa.pt>
2025-09-19 15:16:26 +0100

    spi1 master_pic

---
+++ MPLAB_Examples/SPI.X/build/default/debug/main.o.d
@@ -0,0 +1,5 @@
+build/default/debug/main.o: main.c common.h spi.h
+
+common.h:
+
+spi.h:
---
+++ MPLAB_Examples/SPI.X/common.c
@@ -0,0 +1,24 @@
+/*
+ * File:   common.c
+ * Author: João Duarte
+ *         Gonçalo Antunes
+ *         Gonçalo Batalha
+ *
+ * Created on September 9, 2025, 11:38 PM
+ */
+
+
+#include "common.h"
+#include "../../PIC24_Lib/PIC24FJ256GA702_lib.X/PIC24FJ256GA702_lib.h"
+
+void setup(void){
+    // Insert your setup code here, to run once:
+    
+    return;
+}
+
+void loop(void){
+    // Insert your loop code here, to run repeatedly:
+    
+    return;
+}
